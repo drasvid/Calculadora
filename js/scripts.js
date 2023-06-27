1fbe2e53918cbff57af5554a1621e8481b6d3a77
D4v1d_4gu1l4r <da70699@gmail.com>
2023-06-26 19:53:13 -0500

    Empiezo a restructurar

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,12 @@ const keys=document.querySelectorAll('.keys button');
 const add =document.querySelector('.add');
 const saved =document.querySelector('.saved');
 
+const suma=32+2;
+
+console.log(typeof(suma))
+
+
+
 
 
 
@@ -10,35 +16,53 @@ keys.forEach(key=>{
 
     key.addEventListener('click', event=>{
 
-            const key_contend=key.innerHTML;
-
-            if (key_contend==='AC') {
-
-                add.textContent=0;
-                saved.textContent='';          
-                
-            }
-            else{
-
-            add.textContent=key_contend;
-
-            saved.textContent=saved.textContent+add.textContent;
-
-            add.textContent='';
-
-            }
-
-        const qw=console.log(+saved.textContent)
-
-        console.log(typeof(qw))
+        print(key);
 
     })
-
-
 });
 
 
 
+function print(key){
+
+    const key_contend=key.innerHTML;
+
+    if (key_contend==='AC') {
+
+        add.textContent=0;
+        saved.textContent='';          
+        
+    }
+    else{
+
+    add.textContent=key_contend;
+
+    saved.textContent=saved.textContent+add.textContent;
+
+    add.textContent='';
+
+    }
+
+    operative()
+
+};
+
+
+function operative(){
+
+    const igual=document.querySelector('.igual');
+    const valor=saved.textContent;
+
+    igual.addEventListener('click',()=>{
+
+    parseInt(valor,10)
+    
+    console.log(typeof(valor))
+
+
+    })
 
 
 
+
+}
